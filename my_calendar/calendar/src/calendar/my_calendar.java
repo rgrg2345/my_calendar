91cae80d2b52801c7343e5fa379ff13ce5ba473c
rgrg2345 <s358tw@gmail.com>
2014-07-19 13:49:11 +0800

    Initail commit

---
+++ my_calendar/calendar/src/calendar/my_calendar.java
@@ -0,0 +1,71 @@
+package calendar;
+
+public class my_calendar {
+
+	//計算當年前總共有多少天
+	public static int yearday(int year)
+	{
+		int day=0;
+		int baseyear=1900;//以1900為基準
+		for(;year>baseyear;baseyear++)
+			day+=365+((Isleap(baseyear)==true)?1:0);
+		return day;
+	}
+	
+	//判斷閏年
+	public static boolean Isleap(int year)
+	{
+		return ((year%4==0&&year%100!=0)||(year%400==0));
+	}
+	
+	//指定月份前的天數總和
+	public static int monthday(int year ,int month)
+	{
+		int cntday=0;
+		for(int i=1;i<month;i++)
+			cntday+=getmonthday(year,i);
+		return cntday;
+	}
+	
+	//當月月份天數
+	public static int getmonthday(int year,int month)
+	{
+		int Month[]={31,28,31,30,31,30,31,31,30,31,30,31};
+		return Month[month-1]+((Isleap(year)==true&&month==2)?1:0);
+	}
+	//得到當月1號星期幾
+	public static int getWeek(int year,int month)
+	{
+		int allday=yearday(year)+monthday(year,month);
+		int week=(allday+1)%7;
+		return week;
+	}
+	public static void calendar(int year ,int month)
+	{
+		int monthday=getmonthday(year,month)+1;
+		int week=getWeek(year,month);//1號星期幾
+		System.out.println(year+"/"+month  +"    week:"+week);
+		System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+		for(int i=0;i<week*4;i++)
+			System.out.print(" ");
+		for(int i=1;i<monthday;i++)
+		{
+			System.out.print(" ");
+			System.out.print(i);
+			if(i<10) System.out.print(" ");
+			week=(++week)%7;
+			if(week==0)
+				System.out.println("");
+			else
+				System.out.print(" ");
+		}
+	}
+	public static void main(String args[] )
+	{
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		System.out.println("Please input year and month.");
+		int year=scanner.nextInt();
+		int mon=scanner.nextInt();
+		calendar(year,mon);
+	}
+}
